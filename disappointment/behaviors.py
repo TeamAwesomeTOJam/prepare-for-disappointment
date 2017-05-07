6679d9d0f00f564af3fe3960f0218a9cb5aaa5e7
Jonathan Doda <jonathan@jdoda.ca>
2017-05-07 15:50:49 -0400

    Don't let walk cancel the shoot animation.

--- disappointment/behaviors.py
+++ disappointment/behaviors.py
@@ -358,7 +358,7 @@ class PlayerAnimationChooser(Behavior):
                 entity.handle('play_animation', 'character-land')                
             if entity.dir == "none" and entity.animation_name == 'character-walk':
                 entity.handle('play_animation', 'character-idle')
-            elif entity.dir != "none" and entity.animation_name != 'character-land':
+            elif entity.dir != "none" and entity.animation_name not in ('character-land', 'character-shoot'):
                 entity.handle('play_animation', 'character-walk')
 
 class HurtBadGuy(Behavior):
